581a8a2f93fbb5174e9c590a57f3029831c28cbb
gustaxcx <yangsincera@gmail.com>
2026-01-05 16:28:34 -0300

    feat props soul

---
+++ soul_props_by_joao/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+ --- PELÚCIAS
+data_file 'DLC_ITYP_REQUEST' 'stream/metas/labubu_by_joao.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/metas/pets_by_joao.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/metas/elle_by_joao.ytyp'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/metas/fivenights_by_joao.ytyp'
+dependency '/assetpacks'
